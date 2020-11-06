9ec817ffaa96ab9ce7a2684d2c355ce1690597e4
David Taylor <david@taylorhq.com>
2020-11-06 22:31:28 +0000

    Update spec

--- spec/lib/reporter/process_spec.rb
+++ spec/lib/reporter/process_spec.rb
@@ -24,40 +24,11 @@ module DiscoursePrometheus
         :v8_heap_count, :v8_physical_size, :pid, :rss, :thread_count)
     end
 
-    describe "with readonly mode cleanup" do
-      after do
-        Discourse.disable_readonly_mode(Discourse::PG_FORCE_READONLY_MODE_KEY)
-        Discourse.clear_readonly!
-      end
+    it "can collect failover data" do
+      metric = Reporter::Process.new(:web).collect
 
-      it "can collect readonly data from the redis keys" do
-        metric = Reporter::Process.new(:web).collect
-
-        Discourse::READONLY_KEYS.each do |k|
-          expect(metric.readonly_sites_count[key: k]).to eq(0)
-        end
-
-        Discourse.enable_readonly_mode(Discourse::PG_FORCE_READONLY_MODE_KEY)
-
-        metric = Reporter::Process.new(:web).collect
-        Discourse::READONLY_KEYS.each do |k|
-          expect(metric.readonly_sites_count[key: k]).to eq(k == Discourse::PG_FORCE_READONLY_MODE_KEY ? 1 : 0)
-        end
-      end
-
-      it "can collect recently readonly data" do
-        freeze_time
-
-        metric = Reporter::Process.new(:web).collect
-        expect(metric.readonly_sites_count[{ key: 'redis_recently_readonly' }]).to eq(0)
-        expect(metric.readonly_sites_count[{ key: 'postgres_recently_readonly' }]).to eq(0)
-
-        Discourse.received_redis_readonly!
-
-        metric = Reporter::Process.new(:web).collect
-        expect(metric.readonly_sites_count[{ key: 'redis_recently_readonly' }]).to eq(1)
-        expect(metric.readonly_sites_count[{ key: 'postgres_recently_readonly' }]).to eq(0)
-      end
+      expect(metric.active_record_failover_count).to eq(0)
+      expect(metric.redis_failover_count).to eq(0)
     end
   end
 end

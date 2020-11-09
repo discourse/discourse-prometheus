cd3c0a2cc98f784fe205575c5b2819190e16a2e8
David Taylor <david@taylorhq.com>
2020-11-09 09:47:32 +0000

    Update lib/internal_metric/process.rb

--- lib/internal_metric/process.rb
+++ lib/internal_metric/process.rb
@@ -15,7 +15,7 @@ module DiscoursePrometheus::InternalMetric
       deferred_jobs_queued: "Number of jobs queued in the deferred job queue",
       active_record_connections_count: "Total number of connections in ActiveRecord's connection pools",
       active_record_failover_count: "Count of ActiveRecord databases in a failover state",
-      redis_failover_count: "Count of redis backends in a failover state"
+      redis_failover_count: "Count of Redis servers in a failover state"
     }
 
     COUNTERS = {

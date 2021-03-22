2135ab46bb46c8e0043962d15a327876311fab4b
Martin Brennan <mjrbrennan@gmail.com>
2021-03-22 13:56:35 +1000

    SECURITY: /metrics accessible from public IP addresses (#19)
    The regular expression to detect private IP addresses did not always
    detect them successfully. Changed to use ruby's in-built
    IPAddr.new(ip_address).private? method instead which does the same thing
    but covers all cases.

--- lib/middleware/metrics.rb
+++ lib/middleware/metrics.rb
@@ -20,12 +20,10 @@ module DiscoursePrometheus
 
     private
 
-    PRIVATE_IP = /^(127\.)|(192\.168\.)|(10\.)|(172\.1[6-9]\.)|(172\.2[0-9]\.)|(172\.3[0-1]\.)|(::1$)|([fF][cCdD])/
-
     def is_private_ip?(env)
       request = Rack::Request.new(env)
       ip = IPAddr.new(request.ip) rescue nil
-      !!(ip && ip.to_s =~ PRIVATE_IP)
+      !!(ip && (ip.private? || ip.loopback?))
     end
 
     def is_trusted_ip?(env)

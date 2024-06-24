5b6bcda8bc5139c714d2a790914237042c3a3110
Bianca Nenciu <nenciu.bianca@gmail.com>
2024-06-24 19:18:43 +0300

    FEATURE: Export metric for highest sequence usage
    Plugin already exports `postgres_highest_sequence`, but the value cannot
    be reliable used for alerts because it is not linked with the maximum
    value of the column that uses it.
    
    In other words, a sequence may be used with an `integer` or a `bigint`
    column, which have very different maximum values. The highest
    `last_value` reported may be cause of concern for an `integer` column
    because it is close to the limit, but not a problem for a `bigint`
    column.

--- spec/lib/internal_metric/global_spec.rb
+++ spec/lib/internal_metric/global_spec.rb
@@ -113,8 +113,12 @@ module DiscoursePrometheus::InternalMetric
       expect(metric.postgres_highest_sequence).to be_a_kind_of(Hash)
       expect(metric.postgres_highest_sequence[{ db: "default" }]).to be_present
 
+      expect(metric.postgres_highest_sequence_usage).to be_a_kind_of(Hash)
+      expect(metric.postgres_highest_sequence_usage[{ db: "default" }]).to be_present
+
       expect do metric.collect end.not_to change {
         metric.class.class_variable_get(:@@postgres_highest_sequence_last_check)
+        metric.class.class_variable_get(:@@postgres_highest_sequence_usage_last_check)
       }
     end
   end
